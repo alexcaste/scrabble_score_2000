3cfd796bda0ce48e4f3c5faadb4b6b88bda7cbb4
james robert williams & alex caste <student@epicodus.com>
2015-04-21 11:32:03 -0700

    add second spec

--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -7,4 +7,9 @@ describe('String#scrabble') do
   it("returns a scrabble score for a letter") do
     expect("a".scrabble()).to(eq(1))
   end
+
+  #it takes a word and returns it as a scrabble score
+  it("takes a word and returns a scarbble score") do
+    expect("hello".scrabble()).to(eq(8))
+  end
 end
