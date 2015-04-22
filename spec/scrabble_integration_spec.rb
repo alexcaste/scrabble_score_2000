ece380b2f3f00e68340c3dc916aaf3e2e6ce8339
alex caste & Jim Patt <student@epicodus.com>
2015-04-22 11:36:37 -0700

    passed first integration spec

--- spec/scrabble_integration_spec.rb
+++ spec/scrabble_integration_spec.rb
@@ -5,8 +5,8 @@ Capybara.app = Sinatra::Application
   describe('the scrabble path', {:type => :feature}) do
     it("processes the user entry and returns the scrabble score") do
     visit('/')
-    fill_in('word_input' , :with => "word")
-    cick_button('Send')
-    expect(page).to have_content(8)
+    fill_in('score' , :with => 'word')
+    click_button('Hit It!')
+    expect(page).to have_content('8')
     end
   end
